bb3740bdf0ecc35999da7219c1e6bb338d5887eb
pickx <pickyaxe@gmail.com>
2019-12-18 15:54:14 +0200

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,20 @@ extern crate scan_fmt;
 extern crate aoc_runner_derive;
 extern crate itertools;
 
-#[doc(inline)]
-pub use std;
-
 use aoc_runner_derive::aoc_lib;
 
+mod opcode;
 
+mod day1;
+//mod day 2;
+mod day4;
+mod day3;
+//mod day4;
 mod day5;
 mod day6;
 //mod day7;
 mod day8;
 mod day10;
 mod day12;
-
 
 aoc_lib! { year = 2019 }
