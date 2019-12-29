e351b54142e247b0576f779f35f1a371452eeb52
yuval <pickyaxe@gmail.com>
2019-12-29 11:58:03 +0200

    day15 part1 finished - managed to implement a DFS to programatically map the maze. it was a lot easier than I thought.

--- src/day15.rs
+++ src/day15.rs
@@ -4,7 +4,7 @@ use crate::opcode::*;
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use itertools::Itertools;
-
+use crate::day15::MoveResult::{MoveSuccess, HitWall};
 
 #[aoc_generator(day15)]
 pub fn input_generator(input: &str) -> Vec<isize> {
@@ -67,10 +67,43 @@ fn draw(image: Vec<Vec<char>>) {
     }
 }
 
-enum MovementResult {
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum MoveResult {
     HitWall,
     MoveSuccess,
     FoundOxygenSystem,
+}
+
+impl MoveResult {
+    fn to_corresponding_tile(&self) -> Tile {
+        match self {
+            MoveResult::HitWall => Tile::Wall,
+            MoveResult::MoveSuccess => Tile::Empty,
+            MoveResult::FoundOxygenSystem => Tile::OxygenSystem,
+        }
+    }
+
+}
+
+impl From<isize> for MoveResult {
+    fn from(response_code: isize) -> Self {
+        match response_code {
+            0 => MoveResult::HitWall,
+            1 => MoveResult::MoveSuccess,
+            2 => MoveResult::FoundOxygenSystem,
+            x => panic!("Got unsupported output {}", x),
+        }
+    }
+}
+
+impl From<MoveResult> for isize {
+    fn from(move_result: MoveResult) -> Self {
+        match move_result {
+            MoveResult::HitWall => 0 ,
+            MoveResult::MoveSuccess => 1,
+            MoveResult::FoundOxygenSystem => 2,
+        }
+    }
 }
 
 fn movement_code_to_direction(movement_code: isize) -> Pos {
@@ -83,22 +116,80 @@ fn movement_code_to_direction(movement_code: isize) -> Pos {
     }
 }
 
-fn positions_from(pos: Pos, maze: &HashMap<Pos, Tile>) -> Vec<_>{
-    let (x, y) = (pos.x, pos.y);
-    let positions = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)];
+
+fn positions_from(pos: Pos) -> Vec<(Pos, isize)>{
+    let (x, y) = (pos.0, pos.1);
+
+    let positions = [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)];
     let movement_codes = [1_isize, 2, 3, 4];
 
-     positions
-        .iter()
-        .zip(movement_codes.iter())
-        .filter(|&(pos, _)| !maze.contains_key(pos))
-        .collect_vec()
+    positions.iter().cloned()
+        .zip(movement_codes.iter().cloned())
+        .collect()
 }
 
-fn dfs(maze: &mut HashMap<Pos, Tile>, runner: from: Pos, distance: usize) {
+fn advance_runner_until_output(runner: &mut OpcodeRunner, input: isize) -> MoveResult {
+    runner.push_input(input);
+    let got_output = false;
+
+    loop {
+        let next_opcode = runner.parse_cur_opcode();
+
+        let got_output = runner.exec_opcode(next_opcode);
+
+        if got_output {
+            let output = runner.output().unwrap();
+            return MoveResult::from(output);
+        }
+    }
+}
+
+fn dfs(maze: &mut HashMap<Pos, Tile>,
+       distances: &mut HashMap<Pos, usize>,
+       cur_pos: Pos,
+       mut runner: OpcodeRunner,
+       code_to_send: isize,
+       distance: usize) {
+
+    //first, check if we've already been here.
+    if let Some(&tile) = maze.get(&cur_pos) {
+        if tile == Tile::Wall {
+            //we've been here and it's a wall, so we can't advance any further.
+            // no need to bother with this position
+            return;
+        }
+
+        //we've already been here so it's safe to unwrap()
+        let best_known_distance_to_here = distances.get(&cur_pos).unwrap();
+        if &distance >= best_known_distance_to_here {
+            //we've been here but haven't been able to do any better, distance-wise.
+            //this also means that we've mapped all positions from this position
+            return;
+        }
+    }
+
+    //now try stepping to this location.
+    let response = advance_runner_until_output(&mut runner, code_to_send);
+    if response == MoveResult::HitWall {
+        maze.insert(cur_pos, Tile::Wall);
+        //then we can't advance any further
+        return;
+    }
+
+    let tile_here = response.to_corresponding_tile();
+    maze.entry(cur_pos).or_insert(tile_here);
+    distances.insert(cur_pos, distance);
+
+    if response == MoveResult::HitWall {
+        //then we can't advance any further
+        return;
+    }
 
 
-
+    let adjacent_positions = positions_from(cur_pos);
+    for (position, code) in adjacent_positions {
+        dfs(maze, distances, position, runner.clone(),code, distance + 1);
+    }
 
 }
 
@@ -108,105 +199,28 @@ fn dfs(maze: &mut HashMap<Pos, Tile>, runner: from: Pos, distance: usize) {
 #[aoc(day15, part1)]
 fn part1(mem: &[isize]) -> usize {
 
-//    let mut maze: HashMap<Pos, Tile> = HashMap::new();
-//    let mut runner = OpcodeRunner::new(mem);
-//    runner.set_input_consume_mode(InputMode::SingleInput);
-//    let starting_position = (0, 0);
-//    let mut robot_pos: Pos = starting_position;
-//
-//
-//
-//    let mut lookup_table: HashMap<char, isize> = HashMap::new();
-//    lookup_table.insert('n', 1);
-//    lookup_table.insert('N', 1);
-//    lookup_table.insert('s', 2);
-//    lookup_table.insert('S', 2);
-//    lookup_table.insert('w', 3);
-//    lookup_table.insert('W', 3);
-//    lookup_table.insert('e', 4);
-//    lookup_table.insert('E', 4);
-//
-//    loop {
-//        maze.insert(robot_pos, Tile::Robot);
-//
-//        let image = image(&maze, robot_pos);
-//        draw(image);
-//
-//        let (x, y) = (robot_pos.0, robot_pos.1);
-//        let positions = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)];
-//        let movement_codes = [1_isize, 2, 3, 4];
-//        let mut unexplored_positions = positions
-//            .iter()
-//            .zip(movement_codes.iter())
-//            .filter(|&(pos, _)| !maze.contains_key(pos))
-//            .collect_vec();
-//
-//
-//        for (_, movement_code) in unexplored_positions {
-//
-//            let movement: Pos = match movement_code {
-//                1 => (0, -1),
-//                2 => (0, 1),
-//                3 => (-1, 0),
-//                4 => (1, 0),
-//                _ => panic!("Illegal movement"),
-//            };
-//
-//            runner.push_input(*movement_code);
-//            let got_output = false;
-//
-//            let movement_result: MovementResult = loop {
-//                let next_opcode = runner.parse_cur_opcode();
-//
-//                let got_output = runner.exec_opcode(next_opcode);
-//
-//                if got_output {
-//                    break match runner.output().unwrap() {
-//                        0 => MovementResult::HitWall,
-//                        1 => MovementResult::MoveSuccess,
-//                        2 => MovementResult::FoundOxygenSystem,
-//                        x => panic!("Got unsupported output {}", x),
-//                    };
-//                }
-//            };
-//
-//            let new_pos = (robot_pos.0 + movement.0, robot_pos.1 + movement.1);
-//
-//            match movement_result {
-//                MovementResult::HitWall => {
-//                    maze.insert(new_pos, Tile::Wall);
-//                },
-//
-//                MovementResult::MoveSuccess => {
-//                    maze.insert(robot_pos, Tile::Empty);
-//                    robot_pos = new_pos;
-//                }
-//
-//                MovementResult::FoundOxygenSystem => {
-//                    maze.insert(new_pos, Tile::OxygenSystem);
-//                    let oxygen_system_pos = new_pos;
-////                return manhattan_distance(starting_position, new_pos);
-//                    println!("Found OxygenSystem at ({}, {})", new_pos.0, new_pos.1);
-////                    return dfs(starting_position, oxygen_system_pos);
-//                    return 0;
-//                }
-//            }
-//
-//
-//        }
-//
-//
-////        let movement_code = loop {
-////            if let Ok(code) = OpcodeRunner::ask_for_input(&lookup_table) {
-////                break code
-////            }
-////        };
+    let starting_position = (0, 0);
+
+    let mut maze: HashMap<Pos, Tile> = HashMap::new();
+    maze.insert(starting_position, Tile::Empty);
+
+    let mut distances: HashMap<Pos, usize> = HashMap::new();
+    distances.insert(starting_position, 0);
+
+    let mut runner = OpcodeRunner::new(mem);
+    runner.set_input_consume_mode(InputMode::SingleInput);
 
 
+    let adjacent_positions = positions_from(starting_position);
+    for (position, code) in adjacent_positions {
+        dfs(&mut maze, &mut distances, position, runner.clone(),code, 1);
+    }
 
+    for (pos, dist) in distances {
+        if maze[&pos] == Tile::OxygenSystem {
+            return dist;
+        }
+    }
 
-
-
-//}
-
+    usize::max_value()
 }
